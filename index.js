a9a8806e5c8de0c0d8ef7df972410b4f79362fa1
Ricardo Ribeiro <kodii@kodii.eu>
2022-10-21 19:11:05 +0200

    populate and render the menu board

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-import { menuArray } from "./data";
+import { menuArray } from './data.js';
 
-console.log(menuArray)
+const addItemBtn = document.getElementById('add-item-btn')
+console.log(addItemBtn)
+
+document.addEventListener("click", function(e){
+    console.log(e.target.dataset.name)
+})
+
+//function to populate the menu board
+function menuBoard() {
+    let cartItems = ''
+
+    menuArray.forEach(function (item){
+        cartItems += `
+            <div class="menu-items" id="menu-items">
+                <span class="emoji" data-emoji="${item.id}">${item.emoji}</span>
+                <div class="item-data">
+                    <h3 class="item-name" data-name="${item.id}">${item.name}</h3>
+                    <p data-ingredients="${item.id}">${item.ingredients}</p>
+                    <span class="price" data-price="${item.id}">$${item.price}</span>
+                </div>
+                <button id="add-item-btn">+</button>
+            </div>
+        `
+    })
+    return cartItems
+}
+
+//render menu items
+function render() {
+    document.getElementById("feed").innerHTML = menuBoard()
+}
+
+render()
