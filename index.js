cc883d792ab0179ce23baa115bcf4ab08f50e4fa
Codzign <11535653+kodiii@users.noreply.github.com>
2022-10-22 08:58:40 +0200

    handle add item to cart

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import { menuArray } from './data.js';
 
-const addItemBtn = document.getElementById('add-item-btn')
-console.log(addItemBtn)
-
-document.addEventListener("click", function(e){
-    console.log(e.target.dataset.name)
+document.addEventListener("click",function(e){
+    //console.log(e.target.dataset.name)
+    if (e.target.dataset.addItemBtn) {
+        addItem(e.target.dataset.addItemBtn)
+        console.log(e.target.dataset.name)
+    } else {
+        console.log('wrong')
+    }
 })
 
-//function to populate the menu board
+//function to populate the menu items
 function menuBoard() {
-    let cartItems = ''
+    let menuItems = ''
 
     menuArray.forEach(function (item){
-        cartItems += `
+        menuItems += `
             <div class="menu-items" id="menu-items">
                 <span class="emoji" data-emoji="${item.id}">${item.emoji}</span>
                 <div class="item-data">
@@ -20,11 +23,18 @@ function menuBoard() {
                     <p data-ingredients="${item.id}">${item.ingredients}</p>
                     <span class="price" data-price="${item.id}">$${item.price}</span>
                 </div>
-                <button id="add-item-btn">+</button>
+                <button class="add-item-btn" data-addItemBtn="${item.id}">+</button>
             </div>
         `
     })
-    return cartItems
+    return menuItems
+}
+
+function addItem(itemMenuId) {
+    const itemTargetIdObj = menuArray.filter(function (item) {
+        return item.id === itemMenuId
+    })[0]
+    console.log(itemTargetIdObj)
 }
 
 //render menu items
