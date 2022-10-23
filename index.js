850e5af19a976d74cdd29f4844c042a82941109c
Ricardo Ribeiro <kodii@kodii.eu>
2022-10-23 20:12:29 +0200

    bugs additemtocart

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { menuArray } from './data.js';
+const menuCart = document.getElementById("menuCart")
 
 document.addEventListener("click",function(e){
     if (e.target.dataset.additembtn) {
-        addItem(e.target.dataset.additembtn)
+        addItemToCart(e.target.dataset.additembtn)
+        renderCart()
     }
 })
 
@@ -26,19 +28,34 @@ function menuBoard() {
     return menuItems
 }
 
-function addItem(itemMenuId) {
+function addItemToCart(itemMenuId) {
     const itemTargetIdObj = menuArray.filter(function (item) {
         return item.id === parseInt(itemMenuId)
-        //console.log(item.id, item.name, item.ingredients)
     })[0]
-    console.log(itemTargetIdObj)
-    let cartItemsObj = [{
-        name: '',
-        price: 0
-    }]
 
-    /*cartItemsObj.name.push(itemTargetIdObj.name)
-    console.log(cartItemsObj)*/
+    let cartMenu = ''
+    let totalCart = 0
+    totalCart += itemTargetIdObj.price
+
+    cartMenu = `
+            <p>Your order</p>
+            <div class="container-cart">
+                <div class="items">
+                    <div class="items-name-col">
+                        ${itemTargetIdObj.name}
+                        <span class="remove-item" data-remove="${itemTargetIdObj.id}">remove</span>
+                    </div>
+                    <div class="items-price-col">
+                        ${itemTargetIdObj.price}
+                    </div> 
+                </div>
+                <p>Total price: ${totalCart}</p>
+            </div>   
+        `
+    //return cartMenu
+    return menuCart.innerHTML = cartMenu
+
+        //console.log(itemTargetIdObj.price)
 }
 
 //render menu items
@@ -46,4 +63,10 @@ function render() {
     document.getElementById("menuFeed").innerHTML = menuBoard()
 }
 
+function renderCart() {
+    //document.getElementById("menuCart").innerHTML = addItemToCart()
+    console.log('done')
+}
+
 render()
+
