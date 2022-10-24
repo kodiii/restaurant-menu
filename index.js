eef8c9cfa8e3822404f671db963c1d50f2814335
Ricardo Ribeiro <kodii@kodii.eu>
2022-10-24 19:44:16 +0200

    render cart

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-import { menuArray } from './data.js';
-const menuCart = document.getElementById("menuCart")
+import { menuArray } from './data.js'
+
+let cartArray = []
 
 document.addEventListener("click",function(e){
     if (e.target.dataset.additembtn) {
         addItemToCart(e.target.dataset.additembtn)
-        renderCart()
+    } else if (e.target.dataset.remove) {
+        console.log(e.target.dataset.remove)
+        //deleteItem(e.target.dataset.remove)
     }
 })
 
@@ -17,7 +20,7 @@ function menuBoard() {
             <div class="menu-items" id="menu-items">
                 <span class="emoji" data-emoji="${item.id}">${item.emoji}</span>
                 <div class="item-data">
-                    <h3 class="item-name" data-name="${item.id}">${item.name}</h3>
+                    <span class="item-name" data-name="${item.id}">${item.name}</span>
                     <p data-ingredients="${item.id}">${item.ingredients}</p>
                     <span class="price" data-price="${item.id}">$${item.price}</span>
                 </div>
@@ -28,45 +31,56 @@ function menuBoard() {
     return menuItems
 }
 
+//handle add item to cart click and render it
 function addItemToCart(itemMenuId) {
     const itemTargetIdObj = menuArray.filter(function (item) {
         return item.id === parseInt(itemMenuId)
     })[0]
 
+    cartArray.push(itemTargetIdObj)
+
     let cartMenu = ''
     let totalCart = 0
-    totalCart += itemTargetIdObj.price
 
-    cartMenu = `
-            <p>Your order</p>
-            <div class="container-cart">
-                <div class="items">
-                    <div class="items-name-col">
-                        ${itemTargetIdObj.name}
-                        <span class="remove-item" data-remove="${itemTargetIdObj.id}">remove</span>
+    cartArray.forEach(function(item) {
+        cartMenu += `
+                <div class="container-cart">
+                    <div class="cart-items">
+                        <div class="items-name-col">
+                            <span class="item-name">${item.name}</span>
+                            <button data-remove="${item.id}">remove</button>
+                        </div>
+                        <div class="items-price-col">
+                            <span class="price">$${item.price}</span>
+                        </div>
                     </div>
-                    <div class="items-price-col">
-                        ${itemTargetIdObj.price}
-                    </div> 
                 </div>
-                <p>Total price: ${totalCart}</p>
-            </div>   
-        `
-    //return cartMenu
-    return menuCart.innerHTML = cartMenu
+            `
+        totalCart += item.price
+    });
 
-        //console.log(itemTargetIdObj.price)
+    document.getElementById("table-items").innerHTML = cartMenu
+    document.getElementById("total-items-price").innerHTML = `
+        <span class="total-price">Total:</span>
+        <span class="price">$${totalCart}</span>
+    `
+    document.getElementById("menuCart").style.display = 'block'
+    //console.log(cartMenu)
 }
 
 //render menu items
-function render() {
+function renderMenuBoard() {
     document.getElementById("menuFeed").innerHTML = menuBoard()
 }
 
-function renderCart() {
-    //document.getElementById("menuCart").innerHTML = addItemToCart()
-    console.log('done')
+function deleteItem(removeItem) {
+    cartArray.forEach(function(item) {
+        if(removeItem === item.id) {
+            console.log("done")
+        }
+    })
+    console.log(cartArray)
 }
 
-render()
+renderMenuBoard()
 
